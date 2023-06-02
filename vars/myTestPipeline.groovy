a26608378e96b010539f44327aaf49055bcc0f8f
halltony <a.j.hall@blueyonder.co.uk>
2023-06-02 21:44:40 +0100

    convert to shared pipleine

---
+++ vars/myTestPipeline.groovy
@@ -0,0 +1,13 @@
+def call(Map pipelineParams) {
+    pipeline {
+        agent any
+
+        stages {
+            stage('Hello') {
+                steps {
+                    echo 'Hello World'
+                }
+            }
+        }
+    }
+}
